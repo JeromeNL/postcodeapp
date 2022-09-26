a3d31fe96b4ad3282084f841c356f415b7f027bf
jeromenl <jw.kwetters@student.avans.nl>
2022-09-26 15:49:38 +0200

    Buttons in sidebar functional.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'dart:ui';
-
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:external_app_launcher/external_app_launcher.dart';
+import 'package:store_redirect/store_redirect.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +22,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  final InAppReview inAppReview = InAppReview.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,37 +61,73 @@ class FirstScreen extends StatelessWidget {
                 ),
               ),
             ),
+            // Share PlayStore link to a friend by 'share popup'
             ListTile(
-                leading: Icon(Icons.share),
-                title: const Text('Deel deze app'),
-                onTap: () {
-                  print("Knopje 1");
-                }),
-            ListTile(
-                leading: Icon(Icons.reviews),
-                title: const Text('Beoordeel ons'),
-                onTap: () {
-                  print("Knopje 1");
-                }),
-            ListTile(
-                leading: Icon(Icons.info),
-                title: const Text('Meer info'),
-                onTap: () {
-                  print("Knopje 1");
-                },
+              leading: Icon(Icons.share),
+              title: const Text('Deel deze app'),
+              onTap: ()  {
+                Share.share('Ik gebruik deze geweldige app: https://www.joramkwetters.nl/');
+              },
             ),
-            SizedBox(height: MediaQuery.of(context).size.height / 1.75),
+            // User goes to PlayStore, where he can leave a review.
+            ListTile(
+              leading: Icon(Icons.reviews),
+              title: const Text('Beoordeel ons'),
+              onTap: () async {
+                try {
+                   {
+                    StoreRedirect.redirect(
+                        androidAppId: "nl.rtl.videoland.v2",
+                        iOSAppId: "585027354");
+                  }
+                } catch (e) {
+                  print("ERROR!: $e");
+                }
+              },
+            ),
+            // User goes to personal website
+            ListTile(
+              leading: Icon(Icons.info),
+              title: const Text('Meer info'),
+              onTap: () async {
+                Uri url = Uri.parse("https://www.joramkwetters.nl");
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                } else {
+                  throw "Cannot load url";
+                }
+              },
+            ),
+            // User goes to personal Github of JeromeNL
+            SizedBox(height: MediaQuery.of(context).size.height / 1.7),
             Divider(
-              color: Colors.grey[700],
+              color: Colors.grey[900],
               height: 0,
             ),
             Align(
-              alignment: Alignment.bottomCenter,
-              child: ListTile(
-                title: Text(
-                  'An app by JeromeNL',
-                  style: TextStyle(
-                    color: Colors.grey[500],
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontStyle: FontStyle.italic,
+                        ),
+                        text: "An App by JeromeNL",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            Uri url = Uri.parse("https://github.com/JeromeNL");
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw "Cannot load url";
+                            }
+                          },
+                      ),
+                    ],
                   ),
                 ),
               ),
