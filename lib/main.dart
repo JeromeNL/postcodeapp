14ab895f9e570d15d6f237d2c4b6ed50f9b23344
jeromenl <jw.kwetters@student.avans.nl>
2022-09-26 13:19:24 +0200

    View of Sidebar is added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,6 +28,66 @@ class FirstScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[900],
       ),
+      drawer: Drawer(
+        child: ListView(
+          physics: const NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(
+              height: 125,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                ),
+                child: const Center(
+                  child: Text(
+                    "Adres Checker",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.share),
+                title: const Text('Deel deze app'),
+                onTap: () {
+                  print("Knopje 1");
+                }),
+            ListTile(
+                leading: Icon(Icons.reviews),
+                title: const Text('Beoordeel ons'),
+                onTap: () {
+                  print("Knopje 1");
+                }),
+            ListTile(
+                leading: Icon(Icons.info),
+                title: const Text('Meer info'),
+                onTap: () {
+                  print("Knopje 1");
+                },
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 1.75),
+            Divider(
+              color: Colors.grey[700],
+              height: 0,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ListTile(
+                title: Text(
+                  'An app by JeromeNL',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         decoration: BoxDecoration(
           color: Colors.grey[200],
@@ -34,6 +96,3 @@ class FirstScreen extends StatelessWidget {
     );
   }
 }
-
-
-
